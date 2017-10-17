884a768426d1ecc15b47d3c89f352e28ee18ef55
Gopa Vasanth <gopavasanth1999@gmail.com>
2017-10-17 17:52:34 +0530

    pset-2 sol

---
+++ caesar.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<string.h>
+#include<ctype.h>
+
+int main(int argc,string a[])
+{
+    int i;
+
+    if (argc != 2)
+    {
+        printf("/n");
+     return 1;
+    }
+
+  int b = atoi(a[1]);
+
+  if (b<0)
+  {
+      printf("/n");
+      return 1;
+  }
+  else
+  {
+      printf("plaintext:");
+      string s = get_string();
+      printf("ciphertext:");
+      for (i=0;i<strlen(s);i++)
+       if islower(s[i])
+       {
+         s[i] =( (s[i]+(b) )-(97) )%26 +97;
+
+          printf("%c",s[i]);
+
+       }
+       else if isupper(s[i])
+          {
+              s[i] = ((s[i]+(b) )-(65) )%29 + 65;
+
+              printf("%c",s[i]);
+          }
+        else
+        {
+
+            printf("%c",s[i]);
+
+        }
+        return 0;
+  }
+  }
+
+
+
---
+++ initials.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<cs50.h>
+#include <strings.h>
+
+int main(void)
+{
+    int i;
+
+    string name = get_string();
+
+
+    printf("%c", toupper(name[0]));
+
+    for (int i = 0; i < strlen(name); i++)
+    {
+        if (name[i] == ' ')
+        {
+
+            printf("%c", toupper(name[i + 1]));
+        }
+    }
+    printf("\n");
+}
