f2f516e50b5effa929aec9baa9a9f3a6a61a41a3
premasai143 <39990278+premasai143@users.noreply.github.com>
2019-10-11 00:05:13 +0530

    fixed syntax errors

--- caesar.c
+++ caesar.c
@@ -1,56 +1,32 @@
 #include<stdio.h>
-#include<cs50.h>
 #include<string.h>
 #include<ctype.h>
 
-int main(int argc,string a[])
+int main(int argc,char* a[])
 {
-    int i;
-
-    if (argc != 2)
-    {
-        printf("/n");
-     return 1;
-    }
-
-  int b = atoi(a[1]);
-
-  if (b<0)
-  {
-      printf("/n");
-      return 1;
-  }
-  else
-  {
+	  int b,i=0;
       printf("plaintext:");
-      string s = get_string();
+      char s[100];
+      scanf("%s",s);
       printf("ciphertext:");
-      for (i=0;i<strlen(s);i++)
-       {
-           if islower(s[i])
-       {
-         s[i] =( (s[i]+(b) )-(97) )%26 +97;
-
-          printf("%c",s[i]);
-
-       }
-       else if isupper(s[i])
-          {
+      while(s[i]){
+           if (islower(s[i]))
+       		{
+         		s[i] =( (s[i]+(b) )-(97) )%26 +97;
+				 printf("%c",s[i]);
+			}
+			 else if (isupper(s[i]))
+          	{
               s[i] = ((s[i]+(b) )-(65) )%29 + 65;
-
               printf("%c",s[i]);
-          }
-        else
-        {
-
-            printf("%c",s[i]);
-
-        }
+          	}
+        	else
+        	{
+            	printf("%c",s[i]);
+			}
+			i++;
        }
-    printf("\n");
-        return 0;
-  }
-  }
-
-
+	printf("\n");
+	return 0;
+}
 
